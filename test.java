f6209fa98cf4796694ab946bec9c0dfb507c75da
IDongYoung <417020264@qq.com>
2016-12-13 16:41:36 +0800

    ll

--- test.java
+++ test.java
@@ -1,9 +1,6 @@
-select a.id,a.name,a.address,a.n,a.time,b.S_id,b.score from
-				 (select a.id,a.name,a.address,b.n,a.time
+select a.id,a.name,a.address,b.n,a.time,b.i
 				 from Course as a left join
-				      (select User.name as n, Teacher_Course.C_id
-				           from User,Teacher_Course
-				           where User.id=Teacher_Course.T_id)
-				       as b on a.id=b.C_id)
-				  as a left join Student_Course as b
-				  on a.id=b.C_id and b.S_id='201392133';
+				 (select User.name as n,User.id as i,Teacher_Course.C_id
+				 from User,Teacher_Course
+			   where User.id=Teacher_Course.T_id) as b on
+				 a.id = b.C_id;
