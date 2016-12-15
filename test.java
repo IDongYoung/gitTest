b00655167517ff4b2f4d2e21397280ffa83a5fca
IDongYoung <417020264@qq.com>
2016-12-15 16:10:20 +0800

    h

--- test.java
+++ test.java
@@ -1,6 +1,1 @@
-select a.id,a.name,a.address,b.n,a.time,b.i
-				 from Course as a left join
-				 (select User.name as n,User.id as i,Teacher_Course.C_id
-				 from User,Teacher_Course
-			   where User.id=Teacher_Course.T_id) as b on
-				 a.id = b.C_id;
+select * from hello;
