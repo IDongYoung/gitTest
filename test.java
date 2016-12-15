ffff60b38d2afd432daaaa7332b367fb582764a9
IDongYoung <417020264@qq.com>
2016-12-15 16:15:43 +0800

    j

--- test.java
+++ test.java
@@ -1,6 +1,4 @@
 select a.id,a.name,a.address,b.n,a.time,b.i
 				 from Course as a left join
 				 (select User.name as n,User.id as i,Teacher_Course.C_id
-				 from User,Teacher_Course
-			   where User.id=Teacher_Course.T_id) as b on
-				 a.id = b.C_id;
+				 from User,Teacher_Course;
